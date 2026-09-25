31af9f4de0ea39e6eab064038b7e714f6e34ad71
CarlosAla-Estudiante <alaloza.c@gmail.com>
2026-09-26 04:52:40 +1300

    Update Main.java

--- Laboratorio05/Laboratorio05, Ejercicios 1, 2 y 3/Main.java
+++ Laboratorio05/Laboratorio05, Ejercicios 1, 2 y 3/Main.java
@@ -8,13 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Par de String, Integer
         Par<String, Integer> par1 = new Par<>("Edad", 20);
 
-        // Par de Double, Boolean
         Par<Double, Boolean> par2 = new Par<>(15.5, true);
 
-        // Par de Persona, Integer
         Persona persona = new Persona("Ana", 25);
         Par<Persona, Integer> par3 = new Par<>(persona, 100);
 
